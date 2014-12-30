0b3ea99729345a223a85addeac751261ada64515
Michael Kropat <michael.kropat@rackspace.com>
2014-12-30 14:59:44 -0500

    Fix log reader bug

--- VSPerformanceTracker/FSInterface/TailReadingFile.cs
+++ VSPerformanceTracker/FSInterface/TailReadingFile.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Reflection;
 
 namespace VSPerformanceTracker.FSInterface
 {
@@ -68,15 +69,42 @@ namespace VSPerformanceTracker.FSInterface
             private string ReadLine(StreamReader reader)
             {
                 var line = reader.ReadLine();
-                _factory.LastOffset = _fs.Position;
+                _factory.LastOffset = reader.GetPosition(); //FIXME: write PositionStreamReader class and use that
                 return line;
             }
 
             public void Dispose()
             {
-                _factory.LastOffset = _fs.Position;
                 _fs.Dispose();
             }
+        }
+    }
+
+    public static class StreamReaderExtensions
+    {
+        // GetPosition based on code written by Granger Godbold [1].
+        // [1] http://stackoverflow.com/a/17457085/27581
+        public static long GetPosition(this StreamReader reader)
+        {
+            // The current buffer of decoded characters
+            var charBuffer = (char[])GetNonPublicField(reader, "charBuffer");
+
+            // The current position in the buffer of decoded characters
+            var charPos = (int)GetNonPublicField(reader, "charPos");
+
+            // The number of bytes that the already-read characters need when encoded.
+            var numReadBytes = reader.CurrentEncoding.GetByteCount(charBuffer, 0, charPos);
+
+            // The number of encoded bytes that are in the current buffer
+            var byteLen = (int)GetNonPublicField(reader, "byteLen");
+
+            return reader.BaseStream.Position - byteLen + numReadBytes;
+        }
+
+        private static object GetNonPublicField(object target, string fieldName)
+        {
+            const BindingFlags flags = BindingFlags.DeclaredOnly | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField;
+            return target.GetType().InvokeMember(fieldName, flags, null, target, null);
         }
     }
 }
